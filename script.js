47124345b88da76460061be1e311e59a8a341742
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-02 19:39:38 +0200

    Added check to make sure the position exists.

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const gameBoard = function(){
              ["", "", ""],
              ["", "", ""]];
 
-    function changeBoard(position, symbol) {
-        let [x, y] = position;
+    function checkPosition(x, y) {
+        return (x > 2 || y > 2 || x < 0 || y < 0);
+    }
+
+    function changeBoard(x, y, symbol) {
         board[x][y] = symbol;
         console.log(board);
     }
 
-    return { board, changeBoard };
+    return { board, changeBoard, checkPosition };
 }();
 
 const displayController = function() {
@@ -22,10 +25,15 @@ const displayController = function() {
     }
 
     function getPlayerChoice(position) {
-        gameBoard.changeBoard(position, currentPlayer);
-        moveNumber += 1;
-        changeCurrentPlayer();
-        console.log(`Player's ${currentPlayer} turn`);
+        let [x, y] = position;
+        if (gameBoard.checkPosition(x, y)) {
+            console.log("This position doesn't exist");
+        } else {
+            gameBoard.changeBoard(x, y, currentPlayer);
+            moveNumber += 1;
+            changeCurrentPlayer();
+            console.log(`Player's ${currentPlayer} turn`);
+        }
     }
 
     return { symbols, getPlayerChoice };
