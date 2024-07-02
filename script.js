4b7f58c78d0bfc05f0dd67050636b93a119a7cc8
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-02 20:42:00 +0200

    Added check for wins and ties.

--- script.js
+++ script.js
@@ -3,10 +3,45 @@ const gameBoard = function(){
              ["", "", ""],
              ["", "", ""]];
 
+    function _checkPositionExists(x, y) {
+        return  (x > 2 || y > 2 || x < 0 || y < 0);
+    };
     function checkPosition(x, y) {
-        let exists = (x > 2 || y > 2 || x < 0 || y < 0);
         let taken = (board[x][y] !== "");
-        return (exists || taken);
+        return (_checkPositionExists(x, y) || taken);
+    };
+
+    function checkEndState() {
+        const possibleLines = [[0, 0], [1, 0], [2, 0],
+                               [0, 0], [0, 1], [0, 2],
+                               [0, 0], [0, 2]];
+        const directions =    [[0, 1], [0, 1], [0, 1],
+                               [1, 0], [1, 0], [1, 0],
+                               [1, 1], [1, -1]]; 
+        const symbol = displayController.currentPlayer;
+
+        function checkLine(start, direction) {
+            let [x, y] = start;
+            for (let i = 0; i<=2; i++) {
+                if (board[x][y] !== symbol) {
+                    return false;
+                }
+                x += direction[0];
+                y += direction[1];
+                if (i === 2) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        for (let i = 0; i < possibleLines.length; i++) {
+            if (checkLine(possibleLines[i], directions[i])) {
+                return true;
+            };
+        };
+        return false;
+
     }
 
     function changeBoard(x, y, symbol) {
@@ -14,7 +49,7 @@ const gameBoard = function(){
         console.log(board);
     }
 
-    return { board, changeBoard, checkPosition };
+    return { board, changeBoard, checkPosition, checkEndState };
 }();
 
 const displayController = function() {
@@ -26,19 +61,31 @@ const displayController = function() {
         currentPlayer = (currentPlayer === symbols[0]) ? symbols[1] : symbols[0];
     }
 
+    function _ending() {
+        console.log(`Player ${currentPlayer} won`);
+    }
+    function _tie() {
+        console.log(`Tie`);
+    }
+
     function getPlayerChoice(position) {
         let [x, y] = position;
         if (gameBoard.checkPosition(x, y)) {
             console.log("This position doesn't exist");
         } else {
             gameBoard.changeBoard(x, y, currentPlayer);
+            if (gameBoard.checkEndState()){
+                _ending(currentPlayer);
+            } else if( moveNumber >= 9) {
+                _tie();
+            }
             moveNumber += 1;
             changeCurrentPlayer();
             console.log(`Player's ${currentPlayer} turn`);
         }
     }
 
-    return { symbols, getPlayerChoice };
+    return { symbols, getPlayerChoice, currentPlayer };
 }();
 
 function Player(symbol) {
