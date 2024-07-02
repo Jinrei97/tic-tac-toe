2afa58fc5e52505995fb64eb5ac3bb14a94c583a
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-02 19:28:43 +0200

    Added ability to change the board with the symbol  of the current player.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const gameBoard = function(){
-    this.board = [];
-    for (let i = 0; i<9; i++) {
-        this.board.push("");
+    board = [["", "", ""],
+             ["", "", ""],
+             ["", "", ""]];
+
+    function changeBoard(position, symbol) {
+        let [x, y] = position;
+        board[x][y] = symbol;
+        console.log(board);
     }
 
-    return { board };
+    return { board, changeBoard };
 }();
 
 const displayController = function() {
     symbols = ["X", "O"];
     moveNumber = 1;
-    currentPlayer = (Math.random() < 0.5) ? this.symbols[0] : this.symbols[1];
-    
-    return { symbols };
+    currentPlayer = (Math.random() < 0.5) ? symbols[0] : symbols[1];
+
+    function changeCurrentPlayer() {
+        currentPlayer = (currentPlayer === symbols[0]) ? symbols[1] : symbols[0];
+    }
+
+    function getPlayerChoice(position) {
+        gameBoard.changeBoard(position, currentPlayer);
+        moveNumber += 1;
+        changeCurrentPlayer();
+        console.log(`Player's ${currentPlayer} turn`);
+    }
+
+    return { symbols, getPlayerChoice };
 }();
 
 function Player(symbol) {
